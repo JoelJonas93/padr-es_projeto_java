ab9624f752aa8ea1bc439ad8cddd5488f225764a
DESKTOP-TK0J234\Unity <joel.oliveira@fatec.sp.gov.br>
2021-10-22 17:49:56 -0300

    Implementation Singleton

--- src/main/java/br/com/cod3r/singleton/connectionPool/conn/ConnectionPool.java
+++ src/main/java/br/com/cod3r/singleton/connectionPool/conn/ConnectionPool.java
@@ -4,10 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ConnectionPool {
+	// Criando uma única instância da classe
+	private final static ConnectionPool instance = new ConnectionPool();
+
 	private final static int POOL_SIZE = 2;
 	private List<Connection> connectionsPool;
+
+	public static ConnectionPool getInstance(){
+		return instance;
+	}
 	
-	public ConnectionPool() {
+	private ConnectionPool() {
 		System.out.println("Creating Connection Pool");
 		connectionsPool = new ArrayList<Connection>();
 		for(int i = 0; i < POOL_SIZE; i++) {
